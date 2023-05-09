85ebce1efc445b5ec246d359cb3af32c98013699
Kristin Martin <kcmartin2@mac.com>
2023-05-09 16:22:32 -0700

    draw green circle on r mouse click

--- mouse_drawing.py
+++ mouse_drawing.py
@@ -10,6 +10,9 @@ def draw_circle(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         cv2.circle(img,(x,y),100,(0,255,0),-1)
 
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        cv2.circle(img,(x,y),75,(255,0,0),-1)
+
 cv2.namedWindow(winname='my_drawing')
 
 cv2.setMouseCallback('my_drawing',draw_circle)
